7dbd266127cd294bccc28743741eac0a0a666ede
Will Harris <will.t.harris@protonmail.com>
2022-02-21 09:09:57 -0800

    add data, add csv crate, parse file and print

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), csv::Error> {
+    let mut reader = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
+
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
 }
