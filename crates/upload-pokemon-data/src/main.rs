26689304ad340c1701505c8afdabdaf672d3a846
Will Harris <will.t.harris@protonmail.com>
2022-02-21 10:12:28 -0800

    deserialize csv rows into PokemonCsv struct

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,8 +1,11 @@
+mod pokemon_csv;
+use pokemon_csv::*;
+
 fn main() -> Result<(), csv::Error> {
     let mut reader = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
 
-    for result in reader.records() {
-        let record = result?;
+    for result in reader.deserialize() {
+        let record: PokemonCsv = result?;
         println!("{:?}", record);
     }
 
